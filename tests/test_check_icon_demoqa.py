935a38469a9ea517464e57f9e7c4f0b47ba47174
Aleksandr Lakhtionov <04.vassals.coffees@icloud.com>
2023-03-19 01:47:37 +0300

    Add optimization class DemoQa
    Optimized DemoQa and BasePage classes

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,18 +1,9 @@
 from pages.demoqa import DemoQa
 
-# def test_icon_exist(browser):
-#     browser.get('https://demoqa.com/')
-#     icon = browser.find_element(By.CSS_SELECTOR, "#app > header > a")
-#
-#     if icon is None:
-#         print('Элемент не найден')
-#     else:
-#         print("Элементы найдены")
-
 
 def test_icon_exist(browser):
     demo_qa_page = DemoQa(browser)
     demo_qa_page.visit()
-    demo_qa_page.click_on_the_icon()
+    demo_qa_page.icon.click()
     assert demo_qa_page.equal_url()
-    assert demo_qa_page.exist_icon()
+    assert demo_qa_page.icon.exit()
